af40de6114933f32c9b892e7223bf4ca79fd89d0
admin <admin@admins-MacBook-Pro-2.local>
2023-01-03 18:49:06 +0400

    create move pieces functionality

--- src/components/Chessboard/Chessboard.tsx
+++ src/components/Chessboard/Chessboard.tsx
@@ -29,6 +29,39 @@ for(let p = 0; p < 2; p++ ){
     pieces.push({image: `assets/images/king_${type}.png`, x: 4, y: y})
 }
 
+let activePiece: HTMLElement | null = null
+
+function grabPiece(e: React.MouseEvent){
+    const element = e.target as HTMLElement
+    if(element.classList.contains("chess-piece")){
+        console.log(e)
+
+        const x = e.clientX - 50
+        const y = e.clientY - 50
+        element.style.position = "absolute"
+        element.style.left = `${x}px`
+        element.style.top = `${y}px`
+        activePiece = element
+    }
+}
+
+function movePice(e: React.MouseEvent){
+    const element = e.target as HTMLElement
+    if(activePiece){
+        const x = e.clientX - 50
+        const y = e.clientY - 50
+        activePiece.style.position = "absolute"
+        activePiece.style.left = `${x}px`
+        activePiece.style.top = `${y}px`
+    }
+}
+
+function dropPiece(e: React.MouseEvent){
+    if(activePiece){
+        activePiece = null
+    }
+}
+
 
 
 export default function Chessboard(){
@@ -44,13 +77,13 @@ export default function Chessboard(){
                     image = p.image
                 }
             })
-            board.push(<Tile image={image} number={number} />)
+            board.push(<Tile key={`${j}, ${i}`} image={image} number={number} />)
             
         }
     }
 
     return (
-        <div id='chessboard'>
+        <div onMouseUp={e => dropPiece(e)} onMouseMove={e => movePice(e)} onMouseDown={e => grabPiece(e)} id='chessboard'>
             {board}
         </div>
     )
