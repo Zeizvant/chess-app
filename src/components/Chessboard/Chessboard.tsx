06da6406f817f03b497c2fd89d9ab16578e08b21
admin <admin@admins-MacBook-Pro-2.local>
2023-01-03 20:47:40 +0400

    grid snapping

--- src/components/Chessboard/Chessboard.tsx
+++ src/components/Chessboard/Chessboard.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Tile from '../Tile/Tile';
 import './Chessboard.css'
 
@@ -11,40 +11,47 @@ interface Piece {
     y: number
 }
 
-const pieces: Piece[] = []
+const initialBoardState: Piece[] = []
+
 for(let i = 0; i < 8; i++){
-    pieces.push({image: "assets/images/pawn_b.png", x: i, y: 6})
-    pieces.push({image: 'assets/images/pawn_w.png', x: i, y: 1})
+    initialBoardState.push({image: "assets/images/pawn_b.png", x: i, y: 6})
+    initialBoardState.push({image: 'assets/images/pawn_w.png', x: i, y: 1})
 }
 for(let p = 0; p < 2; p++ ){
     const type = p === 0 ? "b" : "w"
     const y =  p === 0 ? 7: 0
-    pieces.push({image: `assets/images/rook_${type}.png`, x: 0, y: y})
-    pieces.push({image: `assets/images/rook_${type}.png`, x: 7, y: y})
-    pieces.push({image: `assets/images/knight_${type}.png`, x: 1, y: y})
-    pieces.push({image: `assets/images/knight_${type}.png`, x: 6, y: y})
-    pieces.push({image: `assets/images/bishop_${type}.png`, x: 2, y: y})
-    pieces.push({image: `assets/images/bishop_${type}.png`, x: 5, y: y})
-    pieces.push({image: `assets/images/queen_${type}.png`, x: 3, y: y})
-    pieces.push({image: `assets/images/king_${type}.png`, x: 4, y: y})
+    initialBoardState.push({image: `assets/images/rook_${type}.png`, x: 0, y: y})
+    initialBoardState.push({image: `assets/images/rook_${type}.png`, x: 7, y: y})
+    initialBoardState.push({image: `assets/images/knight_${type}.png`, x: 1, y: y})
+    initialBoardState.push({image: `assets/images/knight_${type}.png`, x: 6, y: y})
+    initialBoardState.push({image: `assets/images/bishop_${type}.png`, x: 2, y: y})
+    initialBoardState.push({image: `assets/images/bishop_${type}.png`, x: 5, y: y})
+    initialBoardState.push({image: `assets/images/queen_${type}.png`, x: 3, y: y})
+    initialBoardState.push({image: `assets/images/king_${type}.png`, x: 4, y: y})
 }
 
 export default function Chessboard(){
+    const [activePiece, setActivePiece] = useState<HTMLElement | null>(null)
+    const [gridX, setGridX] = useState(0)
+    const [gridY, setGridY] = useState(0)
+    const [pieces, setPieces] = useState<Piece[]>(initialBoardState)
+
     const chessboardRef = useRef<HTMLDivElement>(null)
 
-    let activePiece: HTMLElement | null = null
 
     function grabPiece(e: React.MouseEvent){
+        const chessboard = chessboardRef.current
         const element = e.target as HTMLElement
-        if(element.classList.contains("chess-piece")){
-            console.log(e)
+        if(element.classList.contains("chess-piece") && chessboard){
+            setGridX(Math.floor((e.clientX - chessboard.offsetLeft) / 100))
+            setGridY(Math.abs(Math.ceil((e.clientY - chessboard.offsetTop - 800) / 100)))
 
             // const x = e.clientX - 50
             // const y = e.clientY - 50
             // element.style.position = "absolute"
             // element.style.left = `${x}px`
             // element.style.top = `${y}px`
-            activePiece = element
+            setActivePiece(element)
         }
     }
 
@@ -79,8 +86,21 @@ export default function Chessboard(){
     }
 
     function dropPiece(e: React.MouseEvent){
-        if(activePiece){
-            activePiece = null
+        const chessboard = chessboardRef.current
+        if(activePiece && chessboard){
+            const x = Math.floor((e.clientX - chessboard.offsetLeft) / 100)
+            const y = Math.abs(Math.ceil((e.clientY - chessboard.offsetTop - 800) / 100))
+            setPieces((value) => {
+                const pieces = value.map(p => {
+                    if(p.x === gridX && p.y === gridY){
+                        p.x = x
+                        p.y = y
+                    }
+                    return p
+                })
+                return pieces
+            })
+            setActivePiece(null)
         }
     }
 
