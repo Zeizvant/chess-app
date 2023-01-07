1828ceb1f0955959a207a1ab53562970707dc7eb
admin <admin@admins-MacBook-Pro-2.local>
2023-01-07 17:41:45 +0400

    create pawn movement rule

--- src/components/Chessboard/Chessboard.tsx
+++ src/components/Chessboard/Chessboard.tsx
@@ -1,6 +1,8 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useRef, useState } from 'react'
 import Tile from '../Tile/Tile';
 import './Chessboard.css'
+import Referee from "../../referee/Referee"
+import { act } from 'react-dom/test-utils';
 
 const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"]
 const verticalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"]
@@ -9,25 +11,41 @@ interface Piece {
     image: string
     x: number
     y: number
+    type: PieceType
+    team: TeamType
+}
+
+export enum TeamType {
+    OPPONENT,
+    OUR
+}
+export enum PieceType {
+    PAWN,
+    BISHOP,
+    KNIGHT,
+    ROOK,
+    QUEEN,
+    KING
 }
 
 const initialBoardState: Piece[] = []
 
 for(let i = 0; i < 8; i++){
-    initialBoardState.push({image: "assets/images/pawn_b.png", x: i, y: 6})
-    initialBoardState.push({image: 'assets/images/pawn_w.png', x: i, y: 1})
+    initialBoardState.push({image: "assets/images/pawn_b.png", x: i, y: 6, type: PieceType.PAWN, team: TeamType.OPPONENT})
+    initialBoardState.push({image: 'assets/images/pawn_w.png', x: i, y: 1, type: PieceType.PAWN, team: TeamType.OUR})
 }
 for(let p = 0; p < 2; p++ ){
-    const type = p === 0 ? "b" : "w"
-    const y =  p === 0 ? 7: 0
-    initialBoardState.push({image: `assets/images/rook_${type}.png`, x: 0, y: y})
-    initialBoardState.push({image: `assets/images/rook_${type}.png`, x: 7, y: y})
-    initialBoardState.push({image: `assets/images/knight_${type}.png`, x: 1, y: y})
-    initialBoardState.push({image: `assets/images/knight_${type}.png`, x: 6, y: y})
-    initialBoardState.push({image: `assets/images/bishop_${type}.png`, x: 2, y: y})
-    initialBoardState.push({image: `assets/images/bishop_${type}.png`, x: 5, y: y})
-    initialBoardState.push({image: `assets/images/queen_${type}.png`, x: 3, y: y})
-    initialBoardState.push({image: `assets/images/king_${type}.png`, x: 4, y: y})
+    const teamType = (p === 0) ? TeamType.OPPONENT : TeamType.OUR
+    const type = teamType === TeamType.OPPONENT ? "b" : "w"
+    const y =  teamType === TeamType.OPPONENT ? 7: 0
+    initialBoardState.push({image: `assets/images/rook_${type}.png`, x: 0, y: y, type: PieceType.ROOK, team: teamType})
+    initialBoardState.push({image: `assets/images/rook_${type}.png`, x: 7, y: y, type: PieceType.ROOK, team: teamType})
+    initialBoardState.push({image: `assets/images/knight_${type}.png`, x: 1, y: y, type: PieceType.KNIGHT, team: teamType})
+    initialBoardState.push({image: `assets/images/knight_${type}.png`, x: 6, y: y, type: PieceType.KNIGHT, team: teamType})
+    initialBoardState.push({image: `assets/images/bishop_${type}.png`, x: 2, y: y, type: PieceType.BISHOP, team: teamType})
+    initialBoardState.push({image: `assets/images/bishop_${type}.png`, x: 5, y: y, type: PieceType.BISHOP, team: teamType})
+    initialBoardState.push({image: `assets/images/queen_${type}.png`, x: 3, y: y, type: PieceType.QUEEN, team: teamType})
+    initialBoardState.push({image: `assets/images/king_${type}.png`, x: 4, y: y, type: PieceType.KING, team: teamType})
 }
 
 export default function Chessboard(){
@@ -35,8 +53,9 @@ export default function Chessboard(){
     const [gridX, setGridX] = useState(0)
     const [gridY, setGridY] = useState(0)
     const [pieces, setPieces] = useState<Piece[]>(initialBoardState)
-
     const chessboardRef = useRef<HTMLDivElement>(null)
+
+    const referee = new Referee()
 
 
     function grabPiece(e: React.MouseEvent){
@@ -90,11 +109,22 @@ export default function Chessboard(){
         if(activePiece && chessboard){
             const x = Math.floor((e.clientX - chessboard.offsetLeft) / 100)
             const y = Math.abs(Math.ceil((e.clientY - chessboard.offsetTop - 800) / 100))
+
+
             setPieces((value) => {
                 const pieces = value.map(p => {
                     if(p.x === gridX && p.y === gridY){
-                        p.x = x
-                        p.y = y
+                        const validMove = referee.isValidMove(gridX, gridY, x, y, p.type, p.team)
+
+                        if(validMove){
+                            p.x = x
+                            p.y = y
+                        }else{
+                            activePiece.style.position = "relative"
+                            activePiece.style.removeProperty('top')
+                            activePiece.style.removeProperty('left')
+                        }
+                        
                     }
                     return p
                 })
