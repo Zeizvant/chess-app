2be97a54c85aa61a0477bf438d5c9dec6b08b934
admin <admin@admins-MacBook-Pro-2.local>
2023-01-07 18:35:27 +0400

    improve pawn rules

--- src/components/Chessboard/Chessboard.tsx
+++ src/components/Chessboard/Chessboard.tsx
@@ -7,7 +7,7 @@ import { act } from 'react-dom/test-utils';
 const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"]
 const verticalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"]
 
-interface Piece {
+export interface Piece {
     image: string
     x: number
     y: number
@@ -114,7 +114,7 @@ export default function Chessboard(){
             setPieces((value) => {
                 const pieces = value.map(p => {
                     if(p.x === gridX && p.y === gridY){
-                        const validMove = referee.isValidMove(gridX, gridY, x, y, p.type, p.team)
+                        const validMove = referee.isValidMove(gridX, gridY, x, y, p.type, p.team, value)
 
                         if(validMove){
                             p.x = x
